33d9e31b1eeeda263795e2f822c1d118a73dd955
cloud303-kloucks <kloucks@cloud303.io>
2023-10-07 13:34:53 -0700

    updated notify slack with example pipeline link and added example events

--- cicd/slack-notify/app.py
+++ cicd/slack-notify/app.py
@@ -2,6 +2,13 @@ import requests
 import json
 
 webhook_url = ""
+pipeline_link_ex = """
+https://us-east-1.console.aws.amazon.com/codesuite/codepipeline/pipelines/<PIPELINE-NAME>/executions/fd70db0f-26f6-4899-b199-8a26957fc4bd/timeline?region=us-east-1&referer_source=codestar-notifications&referer_medium=chatbot
+"""
+
+basic_pipeline_link = """
+https://us-west-2.console.aws.amazon.com/codesuite/codepipeline/pipelines/<PIPELINE-NAME>/view?region=us-west-2
+"""
 def handler(event):
     message = {
         "channel": "<#notification-slack-channel>",
